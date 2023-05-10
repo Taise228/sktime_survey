5484e75e64dd7352cf4c0653565166e59e6f6aa8
Taise228 <yamamototaisei534@gmail.com>
2023-05-10 11:10:52 +0900

    implement rocket

--- time_series.py
+++ time_series.py
@@ -1,4 +1,7 @@
+import numpy as np
 from sktime.datasets import load_basic_motions
+from sktime.classification.kernel_based import RocketClassifier
+from sktime.classification.hybrid import HIVECOTEV2
 
 
 def main():
@@ -11,6 +14,21 @@ def main():
     motion_test_x, motion_test_y = load_basic_motions(split='test', return_type='numpy3d')
 
     print(motion_train_x.shape, motion_train_y.shape, motion_test_x.shape, motion_test_y.shape)
+
+    rocket = RocketClassifier()
+    print('model downloaded')
+
+    # hc2 = HIVECOTEV2()
+
+    rocket.fit(motion_train_x, motion_train_y)
+    print('training complete')
+    # hc2.fit(motion_train_x, motion_train_y)
+
+    y_pred_rocket = rocket.predict(motion_test_x)
+    # y_pred_hc2 = hc2.predict(motion_test_x)
+
+    accuracy_rocket = np.sum(y_pred_rocket == motion_test_y) / y_pred_rocket.shape[0]
+    print(accuracy_rocket)
 
 
 if __name__ == '__main__':
